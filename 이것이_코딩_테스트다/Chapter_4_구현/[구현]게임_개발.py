0241adebd5bf1eaf078e2908c75507d61c8a250c
minyoung <leemin02_@naver.com>
2022-03-31 21:45:00 +0900

    220330: 이것이 코딩테스트다 - 구현

--- 이것이_코딩_테스트다/Chapter_4_구현/[구현]게임_개발.py
+++ 이것이_코딩_테스트다/Chapter_4_구현/[구현]게임_개발.py
@@ -1,1 +1,51 @@
-# 나의 답 첫번째 (20분 소요)
+# 나의 답 첫번째 (첫번째 시도 실패)
+
+
+
+# 문제 해설
+n,m = map(int,input().split())
+x,y,direction = map(int, input().split())
+
+d = [[0] * m for _ in range(n)]
+d[x][y] = 1
+
+array = [list(map(int, input().split())) for _ in range(n)]
+
+dx = [-1,0,1,0]
+dy = [0,1,0,-1]
+
+def turn_left():
+    global direction
+    direction -= 1
+    if direction == -1:
+        direction = 3
+
+count = 1
+turn_time = 0
+while True:
+    turn_left()
+    nx = x + dx[direction]
+    ny = y + dy[direction]
+    
+    if d[nx][ny] == 0 and array[nx][ny] == 0:
+        d[nx][ny] = 1
+        x = nx
+        y = ny
+        count += 1
+        turn_time = 0
+        continue
+    else:
+        turn_time += 1
+    
+    if turn_time == 4:
+        nx = x - dx[direction]
+        ny = y - dy[direction]
+        if array[nx][ny] == 0:
+            x = nx
+            y = ny
+        else:
+            break
+        turn_time = 0
+    
+
+print(count)
