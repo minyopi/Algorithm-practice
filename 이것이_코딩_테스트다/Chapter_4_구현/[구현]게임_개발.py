17443453dec37038f1005d2dfade9c550959c404
minyoung <leemin02_@naver.com>
2022-04-05 16:54:38 +0900

    220405: 이것이 코딩테스트다 - 구현

--- 이것이_코딩_테스트다/Chapter_4_구현/[구현]게임_개발.py
+++ 이것이_코딩_테스트다/Chapter_4_구현/[구현]게임_개발.py
@@ -1,8 +1,55 @@
 # 나의 답 첫번째 (첫번째 시도 실패)
+n,m = map(int, input().split())
+now_n, now_m, d = map(int, input().split())
+map_data = [ list(map(int,input().split())) for _ in range(m) ]
+
+is_visited = [ [0 for _ in range(n)] for _ in range(m) ]
+is_visited[now_n][now_m] = 1
+
+answer = 1
+
+def turn_left():
+    global d
+    d -= 1
+    if d == -1:
+        d = 3
+
+dn = [0, 0, -1, 1]
+dm = [-1, 1, 0, 0]
+
+count_d = 0
+
+while True:
+    temp_n = now_n + dn[d]
+    temp_m = now_m + dm[d]
+
+    if is_visited[temp_n][temp_m] == 1 or map_data[temp_n][temp_m] == 1:
+        if count_d == 4:
+            count = 0
+            for i in range(4):
+                turn_left()
+                if is_visited[temp_n][temp_m] == 1:
+                    now_n = temp_n
+                    now_m = temp_m
+                count += 1
+            if count == 4:
+                break
+                
+
+    if is_visited[temp_n][temp_m] == 0 and map_data[temp_n][temp_m] == 0:
+        now_n = temp_n
+        now_m = temp_m
+        count_d = 0
+        is_visited[now_n][now_m] = 1
+        answer += 1
+
+    turn_left()
+    count_d += 1
+
+print(answer)
 
 
-
-# 문제 해설
+# # 문제 해설
 n,m = map(int,input().split())
 x,y,direction = map(int, input().split())
 
